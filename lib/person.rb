ce459fdb8ba2472bb88d4469995fb87fd273c112
Justin K <justin-kim1@hotmail.com>
2018-02-22 05:50:49 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,33 +1,10 @@
 class Person
-  def initialize(**kwargs)
-    kwargs.each_pair do |method, value|
-      create_accessors(method, value)
-      public_send("#{method}=", value)
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+  def initialize(attributes=nil)
+    if attributes
+      attributes.each do |k,v|
+        self.send("#{k}=", v)
+      end
     end
-  end
-
-  private
-
-  def create_accessors(method, value)
-    # Adds code to every instance
-    # self.class.instance_eval do
-    #   define_method "#{method}=" do |value|
-    #     instance_variable_set("@#{method}", value)
-    #   end
-    #   define_method method do
-    #     instance_variable_get("@#{method}")
-    #   end
-    # end
-    #
-    # Adds code to each instance separately. 2 Instances won't have the same
-    # accessors unless their keys are are the same during initialization
-    instance_eval <<-CODE
-      def #{method}=(v)
-        @#{method} = v
-      end
-      def #{method}
-        @#{method}
-      end
-    CODE
   end
 end
